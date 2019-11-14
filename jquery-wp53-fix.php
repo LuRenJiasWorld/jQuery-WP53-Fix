1d9d45d75b771ed2aea6cce765e03b428c3d7bad
Benjamin Chris <loli@lurenjia.in>
2019-11-14 21:25:30 +0800

    【Release】Version Update

--- jquery-wp53-fix.php
+++ jquery-wp53-fix.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       jQuery-WP53-Fix
  * Description:       Fix the jQuery compatibility issue in WordPress 5.3
- * Version:           1.0.0
+ * Version:           1.1
  * Author:            LuRenJiasWorld
  * Author URI:        https://github.com/LuRenJiasWorld
  * License:           Apache 2.0
