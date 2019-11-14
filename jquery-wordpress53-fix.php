0d07d9ea3db3967a7e806684f57e624309f0064c
Benjamin Chris <loli@lurenjia.in>
2019-11-14 20:56:51 +0800

    Init

---
+++ jquery-wordpress53-fix.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       jQuery-WordPress53-Fix
+ * Description:       Fix the jQuery compatibility issue in WordPress 5.3
+ * Version:           1.0.0
+ * Author:            LuRenJiasWorld
+ * Author URI:        https://github.com/LuRenJiasWorld
+ * License:           Apache 2.0
+ */
+
+// JUST FIX IT WITHIN A SINGLE LINE OF CODE
+add_action( 'admin_head', function() { echo "<script> var $ = jQuery; </script> "; } );
