f9a3531b42c11b9e4d246adf07794a1093f3a2aa
Benjamin Chris <loli@lurenjia.in>
2019-11-14 21:02:50 +0800

    【Style】Format the code

--- jquery-wordpress53-fix.php
+++ jquery-wordpress53-fix.php
@@ -9,4 +9,4 @@
  */
 
 // JUST FIX IT WITHIN A SINGLE LINE OF CODE
-add_action( 'admin_head', function() { echo "<script> var $ = jQuery; </script> "; } );
+add_action("admin_head", function() { echo "<script> var $ = jQuery; </script> "; } );
